bd7f4c5fb2deba5f730e1119cac8e3c584e0e322
Somename <randommail@mail.com>
2024-11-24 21:43:11 +0100

    Linked libcurl

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <curl/curl.h>
+
 #include "chart.h"
 
 using namespace std;
@@ -8,8 +10,25 @@ using namespace sf;
 
 
 RenderWindow window(VideoMode(1200, 700), "chart");
+Chart bitcoin(5, 10, 40000, 100000);
 
 int main(){
+    CURL* curl = curl_easy_init();
+    CURLcode result;
+
+    if(curl){
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+        result = curl_easy_perform(curl);
+
+        if(result == CURLE_OK){
+            curl_easy_cleanup(curl);
+             
+        }else{
+            cerr << "Error: " << curl_easy_strerror(result);
+        }
+
+
+    }
 
     while (window.isOpen())
     {
